8eb6e08281e11ff928bd294613181585329ef49b
rajshah9305 <rajshah9305@gmail.com>
2025-03-09 00:10:16 -0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,19 @@
 // frontend/next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    domains: [],
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Additional webpack configurations can be added here
+    return config;
+  },
 };
+
+module.exports = nextConfig;
