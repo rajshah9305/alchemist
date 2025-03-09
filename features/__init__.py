df97850560c60163e6bb4b03bc738b5ce9179400
rajshah9305 <rajshah9305@gmail.com>
2025-03-09 00:06:26 -0700

    Update __init__.py

--- features/__init__.py
+++ features/__init__.py
@@ -1,6 +1,18 @@
 # features/__init__.py
+"""
+Features package for enhancing code editor experience with fun, interactive elements.
+
+This package contains classes that provide:
+- Easter eggs triggered by specific phrases
+- Time-based personality shifts
+- Code error alerts with monster themes
+"""
+
 from .easter_eggs import EasterEggs
 from .time_based_shifts import TimeBasedShifts
 from .code_monster_alerts import CodeMonsterAlerts
 
 __all__ = ["EasterEggs", "TimeBasedShifts", "CodeMonsterAlerts"]
+
+# Package version
+__version__ = "0.1.0"
